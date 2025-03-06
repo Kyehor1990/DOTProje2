0b73f46a1ba7bbdc1ca43f2df58886d455b685f3
Kyehor1990 <ardauludag2005@gmail.com>
2025-03-06 09:01:58 +0300

    PlayerMovement

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        movement = movement.normalized;
+
+        if (movement != Vector2.zero)
+        {
+            if (movement.x > 0)
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+            else if (movement.x < 0)
+                transform.rotation = Quaternion.Euler(0, 0, 180);
+            else if (movement.y > 0)
+                transform.rotation = Quaternion.Euler(0, 0, 90);
+            else if (movement.y < 0)
+                transform.rotation = Quaternion.Euler(0, 0, -90);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = movement * moveSpeed;
+    }
+}
