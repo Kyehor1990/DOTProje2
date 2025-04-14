2eaa86f0de6e4a84351face900d17cd02cd9fd45
Kyehor1990 <ardauludag2005@gmail.com>
2025-04-14 11:31:52 +0300

    .

--- Assets/Scripts/Dungeon/DungeonExit.cs
+++ Assets/Scripts/Dungeon/DungeonExit.cs
@@ -2,7 +2,15 @@ using UnityEngine;
 
 public class DungeonExit : MonoBehaviour
 {
-        private bool isLocked = true;
+    private bool isLocked = true;
+
+    public Transform cameraTransform;
+    public Vector3 targetPosition;
+    public GameObject player;
+    
+    public bool Dungeon = true;
+
+    public PlayerHealth playerHealth;
 
     void Start()
     {
@@ -19,6 +27,18 @@ public class DungeonExit : MonoBehaviour
         if (other.CompareTag("Player") && !isLocked)
         {
             Debug.Log("Zindandan çıktın!");
+            
+            if (Dungeon && playerHealth.currentHealth > 0) 
+            {
+                Debug.Log("Dungeon'dan çikiş yapildi.");
+                if (cameraTransform != null)
+                {
+                    cameraTransform.position = targetPosition;
+                }
+
+                playerHealth.currentHealth = playerHealth.maxHealth;
+                Dungeon = false;
+            }
         }
     }
 
