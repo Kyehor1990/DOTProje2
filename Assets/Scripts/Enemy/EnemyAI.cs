b0bbf007aab91b5883945e559a434b4fc5271d08
Kyehor1990 <ardauludag2005@gmail.com>
2025-06-06 02:52:41 +0300

    .

--- Assets/Scripts/Enemy/EnemyAI.cs
+++ Assets/Scripts/Enemy/EnemyAI.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
@@ -8,66 +7,76 @@ public class EnemyAI : MonoBehaviour
     [SerializeField] int damage;
     [SerializeField] float attackRange;
     [SerializeField] float attackCooldown;
-    
-    private Transform player;
-    private bool isAttacking = false;
-    private float lastAttackTime;
 
+    private Transform player;
+    private float lastAttackTime;
     private Rigidbody2D rb;
-    
+
+    private Vector2 movement;
+
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         rb = GetComponent<Rigidbody2D>();
-
     }
-    
+
     void Update()
     {
         float distanceToPlayer = Vector2.Distance(transform.position, player.position);
-        
+
         if (distanceToPlayer > attackRange)
         {
-            Vector2 newPosition = Vector2.MoveTowards(rb.position, player.position, moveSpeed * Time.deltaTime);
-            rb.MovePosition(newPosition);
-
+            // Hedef yönü hesapla (her karede)
+            movement = (player.position - transform.position).normalized;
         }
-    }
-    
-   void OnCollisionEnter2D(Collision2D collision)
-{
-    if (collision.gameObject.CompareTag("Player"))
-    {
-        AttackPlayer(collision.gameObject);
-    }
-}
-
-void OnCollisionStay2D(Collision2D collision)
-{
-    if (collision.gameObject.CompareTag("Player"))
-    {
-        AttackPlayer(collision.gameObject);
-    }
-}
-    
-   void AttackPlayer(GameObject player)
-{
-    if (Time.time - lastAttackTime >= attackCooldown)
-    {
-        PlayerHealth health = player.GetComponent<PlayerHealth>();
-        if (health != null)
+        else
         {
-            health.TakeDamage(damage);
-            lastAttackTime = Time.time;
-            StartCoroutine(AttackEffect());
+            movement = Vector2.zero;
         }
     }
-}
-    
+
+    void FixedUpdate()
+    {
+        // Hareketi sabit fizik update'inde uygula
+        if (movement != Vector2.zero)
+        {
+            Vector2 newPosition = rb.position + movement * moveSpeed * Time.fixedDeltaTime;
+            rb.MovePosition(newPosition);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            AttackPlayer(collision.gameObject);
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            AttackPlayer(collision.gameObject);
+        }
+    }
+
+    void AttackPlayer(GameObject player)
+    {
+        if (Time.time - lastAttackTime >= attackCooldown)
+        {
+            PlayerHealth health = player.GetComponent<PlayerHealth>();
+            if (health != null)
+            {
+                health.TakeDamage(damage);
+                lastAttackTime = Time.time;
+                StartCoroutine(AttackEffect());
+            }
+        }
+    }
+
     IEnumerator AttackEffect()
     {
-        isAttacking = true;
         yield return new WaitForSeconds(0.3f);
-        isAttacking = false;
     }
 }
