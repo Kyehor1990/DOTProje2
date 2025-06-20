b3a8f5bfe99228c28c51d39f17669f42943fb117
Kyehor1990 <ardauludag2005@gmail.com>
2025-06-20 18:27:22 +0300

    .

--- Assets/Scripts/Enemy/EnemyAI.cs
+++ Assets/Scripts/Enemy/EnemyAI.cs
@@ -11,53 +11,22 @@ public class EnemyAI : MonoBehaviour
 
     private Transform player;
     private float lastAttackTime;
-    private Rigidbody2D rb;
-
-    private Vector2 movement;
-
     NavMeshAgent agent;
+    
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        agent.speed = moveSpeed;
         agent.updateRotation = false;
         agent.updateUpAxis = false;
         player = GameObject.FindGameObjectWithTag("Player").transform;
-        rb = GetComponent<Rigidbody2D>();
     }
-
-void Update()
-{
-    /*
-    float distanceToPlayer = Vector2.Distance(transform.position, player.position);
-
-    if (distanceToPlayer > attackRange)
-    {
-        // Hedef yönü hesapla (her karede)
-        movement = (player.position - transform.position).normalized;
-    }
-    else
-    {
-        movement = Vector2.zero;
-    }
-
-    // Oyuncuya X ekseninde dön
-    Vector3 scale = transform.localScale;
-    scale.x = player.position.x > transform.position.x ? -Mathf.Abs(scale.x) : Mathf.Abs(scale.x);
-    transform.localScale = scale;*/
-}
 
 
     void FixedUpdate()
     {
         agent.SetDestination(player.position);
-        /*
-        // Hareketi sabit fizik update'inde uygula
-        if (movement != Vector2.zero)
-        {
-            Vector2 newPosition = rb.position + movement * moveSpeed * Time.fixedDeltaTime;
-            rb.MovePosition(newPosition);
-        }*/
     }
 
     void OnCollisionEnter2D(Collision2D collision)
